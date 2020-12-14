c85ad09f69d5bb3ef1a41a3f78f58b6741a3262c
Khiem Nguyen <ex-nguyenvankhiem001@ocft.com>
2020-12-14 22:26:44 +0700

    Add comment to ArrayDemo java class

--- Unit01_Java_basics/Samples/ArrayDemo.java
+++ Unit01_Java_basics/Samples/ArrayDemo.java
@@ -1,14 +1,17 @@
 public class ArrayDemo {
     public static void main(String[] args) {
+        //Khiem added comment here
         int[] anArray;	        // declare an array of integers
 
         anArray = new int[10];	// create an array of integers
 
         // assign a value to each array element and print 
         for (int i = 0; i < anArray.length; i++) {
-            anArray[i] = i;
+            anArray[i] = i + 100;
             System.out.print(anArray[i] + " ");
         }
+        System.out.println();
+        System.out.println();
         System.out.println();
     }
 }
